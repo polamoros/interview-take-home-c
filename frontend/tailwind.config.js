dc92d2f5023857c3673f05fa08bbf3639b7dfb00
polamoros <info@polamoros.com>
2024-08-01 15:15:48 +0200

    Create custom genesy color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        genesy: {
+          50: '#f6f6f6',
+          100: '#e7e7e7',
+          200: '#d1d1d1',
+          300: '#b0b0b0',
+          400: '#888888',
+          500: '#6d6d6d',
+          600: '#5d5d5d',
+          700: '#4f4f4f',
+          800: '#454545',
+          900: '#3d3d3d',
+          950: '#242424',
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
