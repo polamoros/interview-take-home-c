63122ffc3b5f1e6dfd52ea5aa10206bfd2710d09
polamoros <info@polamoros.com>
2024-08-01 12:08:35 +0200

    Extend and unify Lead type for api responses

---
+++ frontend/src/api/types/leads/index.ts
@@ -0,0 +1,11 @@
+export type Lead = {
+  id: number
+  createdAt: string
+  updatedAt: string
+  firstName: string
+  lastName?: string
+  email?: string
+  jobTitle?: string
+  countryCode?: string
+  companyName?: string
+}
