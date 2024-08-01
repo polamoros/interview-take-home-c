026176f80bf98b0a7cbafa3d29817a66acb30c6e
polamoros <info@polamoros.com>
2024-08-01 15:16:45 +0200

    Create Button component

---
+++ frontend/src/components/desing-system/Button.tsx
@@ -0,0 +1,25 @@
+import clsx from 'clsx'
+import { PropsWithChildren } from 'react'
+
+export interface ButtonProps {
+  disabled?: boolean
+  onClick?: () => void
+}
+
+export const Button = ({ onClick, disabled, children }: PropsWithChildren<ButtonProps>) => {
+  return (
+    <button
+      type="button"
+      className={clsx(
+        'flex gap-1 items-center',
+        'rounded-md px-2.5 py-1.5 text-sm font-semibold shadow-sm'
+        //'focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+        //'text-white bg-genesy-600 hover:bg-genesy-400'
+      )}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
